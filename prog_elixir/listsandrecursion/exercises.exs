68c67c891bb79a475fb7e0944b3da5d251865b38
Destry Saul <destrys@gmail.com>
2020-10-03 10:39:21 -0700

    exercise feedback after looking at some solutions

--- prog_elixir/listsandrecursion/exercises.exs
+++ prog_elixir/listsandrecursion/exercises.exs
@@ -3,6 +3,8 @@ defmodule Exercises do
   def all?([], _), do: true
   
   def all?([head| tail], func) do
+    # this will run to the end of the list
+    # but we could short circuit if it's ever false
     func.(head) && all?(tail, func)
   end
 
@@ -24,8 +26,12 @@ defmodule Exercises do
   end
 
 
+  # Instead of index and counter, we could decrement index
+  # and when it hit zero, return
   def split(list, index, counter \\ 0, first \\ [])
 
+  # instead of ++, we could add head via [head|first] then reverse
+  # when it's time to return
   def split([head|tail], index, counter, first) when counter < index do
     split(tail, index, counter + 1, first ++ [head])
   end    
@@ -35,6 +41,12 @@ defmodule Exercises do
   end
 
 
+  # derp, could just use split and take the first element...
+  # also the same thing about decrementing index instead of keeping
+  # a counter
+
+  # also better to use defp _take/3 and def take/2 instead of
+  # havinf default values of parameters that users won't use
   def take(list, index, counter \\ 0)
   
   def take([head|tail], index, counter) when counter < index do
@@ -43,8 +55,11 @@ defmodule Exercises do
 
   def take(_, index, counter) when counter == index, do: []
 
+  
   def flatten([]), do: []
 
+  # a different way to do this is defin flatten(head) for the non-list
+  # instances
   def flatten([head|tail]) when is_list head do
     flatten(head) ++ flatten(tail)
   end
@@ -52,6 +67,9 @@ defmodule Exercises do
   def flatten([head|tail]) do
     [head|flatten(tail)]
   end
+
+  # a different approach can use flatten([[h|t]|tail], result)
+  # you can do [h,t|tail]
  
 end
 
